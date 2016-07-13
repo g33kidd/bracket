abc452b39232c45072103f62a9bfcac0c9e8a0b8
Joshua Kidd <kidd.josh.343@gmail.com>
2016-07-12 22:38:53 -0600

    updated the teams trait

--- app/Traits/UserHasTeams.php
+++ app/Traits/UserHasTeams.php
@@ -33,7 +33,63 @@ trait UserHasTeams
 
 	public function isTeamOwner($team)
 	{
-		
+		return (
+			Team::where('owner_id', '=', $this->getKey())
+				->where('id', '=', $team->id)->first()
+		) ? true : false;
+	}
+
+	public function attachTeam($team, $pivotData = [])
+	{
+		$this->load('teams');
+
+		if(!$this->teams->contains($team)) {
+			$this->teams()->attach($team, $pivotData);
+			if($this->relationLoaded('teams')) {
+				$this->load('teams');
+			}
+		}
+
+		return $this;
+	}
+
+	public function detachTeam($team)
+	{
+		$this->teams()->detach($team);
+		if($this->relationLoaded('teams')) {
+			$this->load('teams');
+		}
+
+		if($this->teams()->count() === 0 || $this->primary_team_id === $team->id) {
+			$this->primary_team_id = null;
+			$this->save();
+
+			if($this->relationLoaded('primaryTeam')) {
+				$this->load('primaryTeam');
+			}
+		}
+
+		return $this;
+	}
+
+	public function attachTeams($teams)
+	{
+		foreach($teams as $team)
+		{
+			$this->attachTeam($team);
+		}
+
+		return $this;
+	}
+
+	public function detachTeams($teams)
+	{
+		foreach($teams as $team)
+		{
+			$this->detachTeam($team);
+		}
+
+		return $this;
 	}
 
 }
