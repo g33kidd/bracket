fa8892a67875afbb1b4e5cfb4078e6d6d2cde629
Joshua Kidd <kidd.josh.343@gmail.com>
2016-08-16 00:31:46 -0500

    removed fillable for now

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,15 +11,6 @@ class User extends Authenticatable
     use HasApiTokens, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'username', 'name', 'email', 'password',
-    ];
-
-    /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
