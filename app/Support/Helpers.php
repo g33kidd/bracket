e8f536fb276167fb18948049caaa0b0ddc9bcfed
Joshua Kidd <kidd.josh.343@gmail.com>
2016-08-17 10:58:27 -0500

    removed default controller, we don't need it

--- app/Support/Helpers.php
+++ app/Support/Helpers.php
@@ -1,5 +1,32 @@
 <?php
 
-function renderPageView($view, $data = ['extra' => 'No extra data.']) {
+// Renders a view with default site-data a view might require.
+function renderView($view = 'home', $extras = ['extra' => 'No extra data.']) {
+	$defaults = [
+		'games'	=> App\Models\Game::all(),
+		'platforms' => App\Models\Platform::all(),
+		'users' => App\Models\User::all(),
+		'user_count' => App\Models\User::all()->count(),
+	];
 
+	// Filter out data based on what the view accesses?
+	// or something similar...
+	
+	// TODO: figure out how to extract needed values? if this is wanted I suppose
+    // renderView('members', ['members', 'teams', 'tournaments']);
+    // renderView('members', 'view', ['members', 'teams', 'tournaments']);
+    
+	$page_data = array_merge($defaults, $extras);
+	return view('site.' . $view, $page_data);
+}
+
+function getViewData($name) {
+	// an array of things a view can access
+	$accessible = [
+		'users', 'games', 'platforms', 'tournaments'
+	];
+
+	// get the data based on name?
+	// return the data as a collection?
+	// do something?
 }
